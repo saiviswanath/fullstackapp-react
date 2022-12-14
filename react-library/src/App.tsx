58a2333765774a41eb597ecadd6083cdd789430c
viswanath.palaparthi <viswanath.palaparthi@gmail.com>
2022-12-14 21:59:26 +0530

    search functionality

--- react-library/src/App.tsx
+++ react-library/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { SeachBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-      <HomePage/>
+      {/*<HomePage/>*/}
+      <SeachBooksPage/>
       <Footer/>
     </div>
 
