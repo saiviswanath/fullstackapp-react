ccc2ce7e62a63248134d83a3db34f9c8fa13a940
viswanath.palaparthi <viswanath.palaparthi@gmail.com>
2022-12-23 21:00:37 +0530

    Add book

---
+++ react-library/src/model/AddBookRequest.ts
@@ -0,0 +1,17 @@
+export class AddBookRequest {
+    title: string;
+    author: string;
+    description: string;
+    copies: number;
+    category: string;
+    img?: string;
+
+    constructor(title: string, author: string, description: string, copies: number, 
+        category: string) {
+            this.title = title;
+            this.author = author;
+            this.description = description;
+            this.copies = copies;
+            this.category = category;
+        }
+}
