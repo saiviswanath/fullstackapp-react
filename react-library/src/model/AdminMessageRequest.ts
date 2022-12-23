1ff4e383f6e4ee57ea48a9ce91b6d20391961699
viswanath.palaparthi <viswanath.palaparthi@gmail.com>
2022-12-23 20:19:41 +0530

    admin messages

---
+++ react-library/src/model/AdminMessageRequest.ts
@@ -0,0 +1,9 @@
+export class AdminMessageRequest {
+    id: number;
+    response: string;
+
+    constructor(id: number, response: string) {
+        this.id = id;
+        this.response = response;
+    }
+}
